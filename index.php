e3470d32959402a7c3eb1f737bd61e8597682d45
Luis Felipe Sosa <luiyisosabazan@gmail.com>
2021-04-24 17:11:31 -0400

    sobre php

---
+++ index.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+ <html lang= "es">
+<head>
+    <meta charset="utf-8">
+ <body>     
+<p> Suma de numeros con php </p>
+<?php
+$primernumero = 5;
+$segundonumero = 5;
+$resultado = $primernumero + $segundonumero;
+
+echo "el resultado de  $primernumero + $segundonumero es= " .$resultado;
+?>
+</body>
+</hmtl>
